9f90055567f22c12b422541dc666dab4835bee23
Vakiyama <vakiyama@outlook.com>
2024-02-07 15:06:11 -0800

    added render connection details

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,27 +3,25 @@ const mysql = require('mysql2/promise');
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfigQoddi = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
-	multipleStatements: false
+  host: 'sql.freedb.tech',
+  user: 'freedb_main_2_2350',
+  password: '*eP89ebRD5#$Kxa',
+  database: 'freedb_comp2350-week2-A01365036',
+  multipleStatements: false,
 };
 
 const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "database1",
-	multipleStatements: false
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'database1',
+  multipleStatements: false,
 };
 
 if (is_qoddi) {
-	var database = mysql.createPool(dbConfigQoddi);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
+  var database = mysql.createPool(dbConfigQoddi);
+} else {
+  var database = mysql.createPool(dbConfigLocal);
 }
 
 module.exports = database;
-		
